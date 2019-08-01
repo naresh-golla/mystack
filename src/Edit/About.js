f0b2819057ca769d32b299b725ebf51f87ccad3b
spicynaresh <spicynaresh.143@gmail.com>
2019-08-01 16:56:26 +0530

    alingment update in about page

--- src/Edit/About.js
+++ src/Edit/About.js
@@ -7,17 +7,21 @@ class About extends Component {
         this.state = {
             activeDiv: false,
             card: [
-                { id: "0", icon: "", tille: "Support my campaign" },
-                { id: "1", icon: "", tille: "View my Projects" },
-                { id: "2", icon: "", tille: "Download my app" },
-                { id: "3", icon: "", tille: "Listen to my music" },
-                { id: "4", icon: "", tille: "Sign up for my news letter" },
-                { id: "5", icon: "", tille: "Visit my Website" },
-                { id: "6", icon: "", tille: "Read my articles" },
-                { id: "7", icon: "", tille: "My tinder Profile" },
-                { id: "8", icon: "", title: "Visit my store" },
-                { id: "9", icon: "", title: "Listen to my podcast" },
-                { id: "10", icon: "", title: "Attend my event" },
+                { id: "0", icon: "fa fa-thumbs-up", title: "Support my campaign" },
+                { id: "1", icon: "fa fa-github", title: "View my Projects" },
+                { id: "2", icon: "fa fa-cloud-download", title: "Download my app" },
+                { id: "3", icon: "fa fa-music", title: "Listen to my music" },
+                { id: "4", icon: "fa fa-hand-o-up", title: "Hire me" },
+                { id: "5", icon: "fa fa-chrome", title: "Visit my Website" },
+                { id: "6", icon: "fa fa-file-text", title: "Read my articles" },
+                { id: "7", icon: "fa fa-user-o", title: "My tinder Profile" },
+                { id: "8", icon: "fa fa-shopping-cart", title: "Visit my store" },
+                { id: "9", icon: "fa fa-podcast", title: "Listen to my podcast" },
+                { id: "10", icon: "fa fa-calendar", title: "Attend my event" },
+                { id: "11", icon: "fa fa-book", title: "Read my Book" },
+                { id: "12", icon: "fa fa-cloud-download", title: "Download my resume" },
+                { id: "13", icon: "fa fa-chrome", title: "Visit my company" },
+                { id: "14", icon: "fa fa-newspaper-o", title: "Sign up for my news letter" },
             ]
         }
     }
@@ -38,21 +42,17 @@ class About extends Component {
                             <p></p>
                             <Row>
                                 <Col md={12} xs={12}>
-                                    <div className="cardP">
-                                        <div className={this.state.activeDiv ? "activecardclass" : "cardclass"} onClick={this.handleClick} >
-                                            <div>img</div>
-                                            <span></span>
-                                        </div>
-                                        <div className="cardclass">
-                                            <div>img</div>
-                                            <span></span>
-                                        </div>
+                                    <div className="cardP" >
+                                        {this.state.card.map(item => {
+                                            return (
+                                                //className={this.state.activeDiv ? "activecardclass" : "cardclass"}
+                                                <div onClick={this.handleClick} className={this.state.activeDiv ? "activecardclass" : "cardclass"} >
+                                                    <div><i style={{ fontSize: "28px" }} className={item.icon}></i></div>
+                                                    <span>{item.title}</span>
+                                                </div>
 
-                                        <div className="cardclass">
-                                            <div>img</div>
-                                            <span></span>
-                                        </div>
-
+                                            )
+                                        })}
                                     </div>
                                 </Col>
 
@@ -61,7 +61,7 @@ class About extends Component {
                     </Col>
                     <Col md={3} xs={1}></Col>
                 </Row>
-            </Container>
+            </Container >
         )
     }
 }
