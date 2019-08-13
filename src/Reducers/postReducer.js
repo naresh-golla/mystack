282effc0e99f4a3b3ceb36187a5fc0a216a7aa42
spicynaresh <spicynaresh.143@gmail.com>
2019-08-13 16:49:35 +0530

    fixed redux issues

--- src/Reducers/postReducer.js
+++ src/Reducers/postReducer.js
@@ -1,10 +1,9 @@
-
-const postReducer = (state = {}, action) => {
-    switch (action.type) {
-        case 'ADD_BIO':
-            return { ...state, action.bio };
-        default:
-            return state;
-    }
-}
-export default postReducer;
+// const postReducer = (state = {}, action) => {
+//     switch (action.type) {
+//         case 'ADD_BIO':
+//             return { ...state, action.bio };
+//         default:
+//             return state;
+//     }
+// }
+// export default postReducer;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 // import 'bootstrap/dist/css/bootstrap.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import postReducer from "./Reducers/postReducer";
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+// import postReducer from "./Reducers/postReducer";
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// const store = createStore(postReducer);
 
-const store = createStore(postReducer);
-
-ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, document.getElementById('root'))
-registerServiceWorker()
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+registerServiceWorker();
