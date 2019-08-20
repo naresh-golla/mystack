132cbd4dd088b82da49eadab09850e533596193c
varun2116 <varun2116@gmail.com>
2019-08-20 09:44:30 +0000

    Http constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
  * HTTP services related constants
  */
 export const httpConstants = {
-    ENDPOINT: "https://3000-c3928910-3c02-4050-b8d0-0ef010ea1660.ws-ap0.gitpod.io/",
+    ENDPOINT: "http://localhost:8000/",
     GET_USER: "getUser/",
 
 };
