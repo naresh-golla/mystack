ad2f1516959af9814d05534201577795682d108b
spicynaresh <spicynaresh.143@gmail.com>
2019-08-29 18:48:41 +0530

    Http path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@
  * HTTP services related constants
  */
 export const httpConstants = {
-    ENDPOINT: "http://localhost:8000/",
+    ENDPOINT: "http://104.155.136.166:8000/",
     GET_USER: "getUser/",
     UPDATE_USER: "updateUser/",
     CREATE_USER: "createUser"
